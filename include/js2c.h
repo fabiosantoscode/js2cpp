88f97173742094a129aed8d9e2e268cb1122cf17
Fábio Santos <fabiosantosart@gmail.com>
2015-10-14 23:18:51 +0100

    When logging floats, don't log all the zeroes and stuff.

--- include/js2c.h
+++ include/js2c.h
@@ -8,6 +8,15 @@ class Console {
         printf("%s ", first.c_str());
         log(rest...);
     }
+    template<typename... Args>
+    static void log(double first, Args... rest) {
+        if (first == (int)first) {
+            printf("%0.0f ", first);
+        } else {
+            printf("%f ", first);
+        }
+        log(rest...);
+    }
     template<typename T, typename... Args>
     static void log(T t, Args... rest) {
         printf("%s ", std::to_string(t).c_str());
@@ -15,6 +24,13 @@ class Console {
     }
     static void log(std::string only) {
         printf("%s\n", only.c_str());
+    }
+    static void log(double only) {
+        if (only == (int)only) {
+            printf("%0.0f \n", only);
+        } else {
+            printf("%f \n", only);
+        }
     }
     template<typename T>
     static void log(T t) {
