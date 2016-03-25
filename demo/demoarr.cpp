2e76c67de6bd2c9b311c4347ca33fd6154820639
Fábio Santos <fabiosantosart@gmail.com>
2016-03-25 12:30:53 +0000

    updating array demo

--- demo/demoarr.cpp
+++ demo/demoarr.cpp
@@ -3,13 +3,16 @@
 
 
 
-int main () {
+
+
+int main (int argc, char* argv[]) {
     js2cpp_init_libuv();
+    js2cpp_init_argv(argc, argv);
     Array<double> * x;
-    x = (new Array<double>({ 1, 2 }));
-    console.log(x, (new Array<double>({ 1, 2 })), (*x)[0], (*(new Array<double>({ 1, 2 })))[0]);
+    x = (new Array<double>({ 1.0f, 2.0f }));
+    console.log(x, (new Array<double>({ 1.0f, 2.0f })), (*x)[0.0f], (*(new Array<double>({ 1.0f, 2.0f })))[0.0f]);
     Array<std::string> * y = (new Array<std::string>({ std::string("lel") }));
-    console.log(y, (*y)[0]);
+    console.log(y, (*y)[0.0f]);
     y->push(std::string("foo"));
     console.log(y);
     js2cpp_run_libuv();
